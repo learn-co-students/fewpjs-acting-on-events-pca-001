d3f7a1f587b0b0a6e2722a35c44bb1143d877651
Andrew Kloecker <lt.kvothe@gmail.com>
2020-07-07 08:55:31 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Your code here
+const dodger = document.getElementById('dodger');
+
+// tried to get it mor DRY but I kept getting undefined for the stylePosition when passing this doger.style.whatever
+// const move = (stylePosition, inc) => {
+//   console.log(stylePosition)
+//   const number = stylePosition.replace("px", "");
+//   const pos = parseInt(number, 10);
+//   stylePosition = `${pos - inc}px`;
+// }
+
+const moveDodgerLeft = () => {
+    const number = dodger.style.left.replace("px", "");
+    const pos = parseInt(number, 10);
+    dodger.style.left = `${pos - 1}px`;
+}
+
+const moveDodgerRight = () => {
+  const number = dodger.style.left.replace("px", "");
+  const pos = parseInt(number, 10);
+  dodger.style.left = `${pos + 1}px`;
+}
